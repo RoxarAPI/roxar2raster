859513f32c25261b3384332b4602061d068a5941
nilscb <Nils.Bonnevie@emerson.com>
2022-06-22 10:19:32 +0200

    Fix: The 90 degrees left rotation is also necessary for float32.

--- roxar2raster/__init__.py
+++ roxar2raster/__init__.py
@@ -182,6 +182,12 @@ def get_surface_float32(project, name, category, stype):
     values = surface.values.astype(np.float32)
     values.fill_value = np.NaN
     values = np.ma.filled(values)
+
+    # Rotate 90 deg left.
+    # This will cause the width of to run along the X axis
+    # and height of along Y axis (starting from bottom.)
+    values = np.rot90(values)
+
     byte_io = io.BytesIO()
     byte_io.write(values.tobytes())
     byte_io.seek(0)
